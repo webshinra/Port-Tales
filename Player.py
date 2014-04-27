14d9f4cbda3fab7716c652ff9c658f3ce220f128
Vincent <vxgmichel@gmail.com>
2014-04-27 14:02:00 +0200

    Both players handled

--- Player.py
+++ Player.py
@@ -44,16 +44,20 @@ class Player(Tile):
 
     def rotate(self, hat):
         self.dir = hat
-        self.view.set_animaion(hat)
+        self.view.set_animation(hat)
 
 
 
 class PlayerView(TileView):
 
-    folder_dict = {(-1,  0) : "red_player_ne",
-                   ( 0,  1) : "red_player_se",
-                   ( 0, -1) : "red_player_nw",
-                   ( 1,  0): "red_player_sw"}
+    folder_dict = {(1, -1,  0) : "red_player_ne",
+                   (1,  0,  1) : "red_player_se",
+                   (1,  0, -1) : "red_player_nw",
+                   (1,  1,  0): "red_player_sw",
+                   (2, -1,  0) : "green_player_ne",
+                   (2,  0,  1) : "green_player_se",
+                   (2,  0, -1) : "green_player_nw",
+                   (2,  1,  0): "green_player_sw"}
 
     ressource_dict = {key: animation(name) for key, name in folder_dict.items()}
     len_animation = min(len(x) for x in ressource_dict.values())
@@ -62,14 +66,18 @@ class PlayerView(TileView):
     def __init__(self, player_id, board_pos, board_id):
         self.board_pos = XY(*board_pos)
         super(PlayerView, self).__init__(self.board_pos, board_id)
+        self.id = player_id
         self.dirty = 2
-        self.animation = self.ressource_dict[0,1]
+        self.animation = None
+        self.set_animation((0,1))
         self.counter = counter(self.len_animation)
         self.image = self.animation[next(self.counter)]
-        self.id = player_id
+
+
 
     def set_animation(self, hat):
-        self.animation = self.ressource_dict[hat]
+        key = (self.id,) + hat
+        self.animation = self.ressource_dict[key]
 
     def convert(self, pos):
         pos = XY(pos.y-pos.x, pos.x+pos.y)
